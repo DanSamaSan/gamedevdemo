5704b06f439135d8c05ebbd2bb2925507d89bb47
Daniel Nelson <daniel.h.nelson@att.net>
2019-09-16 01:18:34 -0500

    stuff

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*global Phaser, window*/
+import BootScene from './scenes/BootScene.js';
+import GameOverScene from './scenes/GameOverScene.js';
+
+class Game extends Phaser.Game {
+  constructor () {
+    super(Config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('GameOverScene', GameOverScene);
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
